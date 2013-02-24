69ab41ecea08ab1be0ea4b776cfc27bcecdc1cc9
Jon Moore <jmoore0812@hotmail.com>
2013-02-23 21:24:53 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jmoore::Application.config.secret_token = '0652e6b5bd062add3c35753065d069089b7b9846668dd16b3a4740cc2d1a86ee9c9e59bbbcf5808118b47f1afebf4239c918a75ac9ac410e985f4fc526637565'
